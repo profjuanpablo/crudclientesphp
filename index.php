07a45e9ba4fa0f5b5886aaa219877dad46a6f6ea
prof Juan <prof.juanpablo@gmail.com>
2021-12-03 19:29:07 -0300

    crud completo clientes

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto BD - CRUD</title>
+</head>
+<body>
+    <!--a = ANCHOR href = HiperRefence -->
+    <a href="listarclientes.php">Clientes</a>
+</body>
+</html>
